f9857e5c609ecf296d520fed655dca8b270a2687
raiki kanno <heathen1030@gmail.com>
2025-05-13 22:47:09 +0900

    fix

--- src/main/java/com/example/nippou/model/User.java
+++ src/main/java/com/example/nippou/model/User.java
@@ -3,8 +3,9 @@ package com.example.nippou.model;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-
+import jakarta.persistence.Table;
 @Entity
+@Table(name = "\"user\"") 
 public class User {
 
     @Id
